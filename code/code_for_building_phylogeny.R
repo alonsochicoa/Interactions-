3ab2acfa08885d423eda2c4748508b93d7cba94a
Oscar Godoy <ogodoy.re@gmail.com>
2017-12-15 11:13:01 +0100

    changes done to obtain phylogenry calibrated in milion years

--- code/code_for_building_phylogeny.R
+++ code/code_for_building_phylogeny.R
@@ -1,21 +1,23 @@
-#This code is to obtain a phylogenetic relationship among species for Petry et al. 2018
+#This code is to obtain a phylogenetic relationship among species for Alonso et al. 201X
 #by using available information in phylomatic and phylocom
-#User: Oscar Godoy Dec 10th 2017
+#User: Oscar Godoy and Alejandro Alonso, Dec 15th 2017
 
-setwd("/Users/oscargodoy/Documents/Post Doc California/AntFitnessSedgwick")
+setwd("/Users/oscargodoy/Documents/Estudiantes/Alejandro Alonso US TFM/phylo_interactions_caracoles")
 
 #first load brranching to create the species topology. 
 library(brranching)
 library(ape)
+library(phytools)
 
 #load the speceis list and build the topology
 focalspecieslist <- read.csv("data/focalspecies.csv")  # lookup table for updating taxonomy
-taxa <- c(as.vector(focalspecieslist$species_name), "Amborella trichopoda", "Magnolia grandiflora")
+taxa <- c(as.vector(focalspecieslist$species), "Amborella trichopoda", "Magnolia grandiflora")
 #build the tree with available information from APGIII. For more info check http://phylodiversity.net/phylomatic/
 topology <- phylomatic(taxa=taxa, get = 'POST')
 # is the topology ok?
 plot(topology, no.margin=TRUE)
-write.tree(topology, file="data/phylo_topology.tre")
+write.tree(topology, file="data/phylo_topology_polytomies.tre") # we have modified by hand the topology to eliminate polytomies in 
+#Amaranthaceae
 
 
 #This file that I am loading comes from downloading phylocom 4.2, and the ages included are according to Wikstrom et al. 2001 PRBS 
@@ -28,7 +30,7 @@ install_github("ropensci/phylocomr") #this is in case needed.
 library(phylocomr)
 phylo <- as.character(ph_bladj(ages = ages_df, phylo = topology2))
 # eliminate "\n" after angiosperms 
-gsub("\n\\b", "", phylo)
+phylo<- gsub("\n\\b", "", phylo)
 #write the phylogeny as a txt
 write.table(phylo, file="data/phylo_calibrated.tre")
 
@@ -38,4 +40,4 @@ library(phytools)
 phylo2 <- read.tree(file="data/phylo_calibrated.tre")
 pruned.phylo <- drop.tip(phylo2, c("amborella_trichopoda", "magnolia_grandiflora"))
 plot(pruned.phylo, no.margin=TRUE)
-write.tree(pruned.phylo, file="data/phylo_calibrated.tre")
+write.tree(pruned.phylo, file="data/phylo_calibrated_without_outgroup.tre")
